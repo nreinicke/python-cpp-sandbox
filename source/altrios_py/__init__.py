92b53ca0e085a4868ad97fc4d89091167e3af0be
nreinick <nicholas.reinicke@nrel.gov>
2022-02-10 15:28:39 -0700

    add example of reading time trace from file

--- source/altrios_py/__init__.py
+++ source/altrios_py/__init__.py
@@ -2,5 +2,12 @@ from ._core import __doc__, __version__, add, subtract, get_rho_air, TimeTrace
 
 import pandas as pd
 
-def read_time_trace_csv():
-    pass
+from pathlib import Path
+
+def time_trace_from_csv(file: str, time_col = "time_s", speed_col = "speed_m__s") -> TimeTrace:
+    path = Path(file)
+    df = pd.read_csv(path)
+    time = df[time_col].values
+    speed = df[speed_col].values
+    trace = TimeTrace(time, speed)
+    return trace
